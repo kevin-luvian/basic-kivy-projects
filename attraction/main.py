122d1c10ea3e174a3e3b72e68e4d7ffbb25e9bd6
kevin-luvian <kevinlh90@gmail.com>
2020-07-04 08:37:59 +0700

    refactor: set gravity force and repulsion

--- attraction/main.py
+++ attraction/main.py
@@ -9,36 +9,30 @@ from kivy.graphics import Rectangle
 from math import sqrt, pow, degrees, atan2
 from random import randint, randrange
 
+GRAVITY = 3
+
 
 class Attractor(Widget):
     pos = ListProperty([0, 0])
-    weight = 0.02
-    repulsion_radius = 20
+    repulsion_radius = 30
 
     def on_touch_move(self, touch):
         if self.collide_point(*touch.pos):
             self.pos = (touch.x - self.center_x, touch.y - self.center_y)
 
-    def rock_acceleration(self, rock):
-        center_pos = self.pos + self.center
-        vector_line = Vector(center_pos[0] - rock.pos[0] + rock.center_x,
-                             center_pos[1] - rock.pos[1] + rock.center_y)
-
-        # if rock.pos[0] + rock.center_x - center_pos[0] < self.repulsion_radius and \
-        #         rock.pos[1] + rock.center_y - center_pos[1] < self.repulsion_radius:
-        #     vector_line *= -0.05
-        #     return vector_line
-
-        vector_line *= self.weight / vector_line.length()
-        return vector_line
-
 
 class Rock(Widget):
-    id = 0
-    path = ListProperty([])
     pos = ListProperty([0, 0])
     acceleration = Vector(0, 0)
     velocity = Vector(0, 0)
+    life_count = 0
+    life_line = 0
+
+    def create_random_pos(self, max_x, max_y):
+        rand_bool = bool(randint(0, 1))
+        rand_width = 0 if rand_bool else randint(0, max_x)
+        rand_height = 0 if not rand_bool else randint(0, max_y)
+        return [rand_width, rand_height]
 
     def get_angle(self, point):
         len_x = point[0] - self.pos[0]
@@ -50,40 +44,63 @@ class Rock(Widget):
         self.acceleration = self.acceleration * 0
 
     def accelerate_to(self, attractor):
-        self.acceleration = self.acceleration + attractor.rock_acceleration(self)
+        center_target = attractor.pos + attractor.center
+        center_pos = self.pos + self.center
+        force = Vector(center_target[0] - center_pos[0],
+                       center_target[1] - center_pos[1])
+        if force.length() <= attractor.repulsion_radius:
+            force *= -1
+
+        gravity_strength = GRAVITY / pow(force.length(), 2)
+        force *= max(min(10, gravity_strength), 0)
+        self.acceleration = self.acceleration + force
+
+    def reset(self):
+        self.life_count = 0
+        self.acceleration *= 0
+        self.velocity *= 0
+        self.pos = self.create_random_pos(self.parent.width, self.parent.height)
 
     def move(self):
-        self.accelerate_once()
-        self.pos = Vector(self.pos) + self.velocity
-        self.path.append(self.pos)
-        if self.pos[0] < 0 or self.pos[0] > self.parent.width:
-            self.pos[0] = abs(self.parent.width - self.pos[0])
-        if self.pos[1] < 0 or self.pos[1] > self.parent.height:
-            self.pos[1] = abs(self.parent.height - self.pos[1])
+        if self.life_count > self.life_line:
+            self.reset()
+        else:
+            self.accelerate_once()
+            self.pos = Vector(self.pos) + self.velocity
+            self.life_count += 1
 
 
 class Runner(Widget):
     rocks = []
     attractors = []
 
+    def create_random_pos(self, max_x, max_y):
+        rand_bool = bool(randint(0, 1))
+        rand_width = 0 if rand_bool else randint(0, max_x)
+        rand_height = 0 if not rand_bool else randint(0, max_y)
+        return [rand_width, rand_height]
+
     def on_start(self):
         print("im starting")
 
     def build(self, *args):
-        for i in range(300):
+        for i in range(100):
             rock = Rock()
-            rock.id = i
-            rock.pos = [randint(self.center_x - 300, self.center_x + 300),
-                        randint(self.center_y - 300, self.center_y + 300)]
-            # rock.pos = [500, 200]
+            rock.pos = [randint(self.center_x - 30, self.center_x + 30),
+                        randint(self.center_y - 30, self.center_y + 30)]
             self.add_widget(rock)
             self.rocks.append(rock)
 
     def update(self, *args):
-        for attractor in self.attractors:
-            for rock in self.rocks:
-                rock.accelerate_to(attractor)
+        if len(self.rocks) < 100:
+            rock = Rock()
+            rock.life_line = 500 + len(self.rocks) * 10
+            rock.pos = self.create_random_pos(self.width, self.height)
+            self.add_widget(rock)
+            self.rocks.append(rock)
         for rock in self.rocks:
+            for attractor in self.attractors:
+                rock.accelerate_to(attractor)
             rock.move()
 
     def on_touch_down(self, touch):
@@ -96,7 +113,7 @@ class Runner(Widget):
 class AttractionApp(App):
     def build(self):
         runner = Runner()
-        Clock.schedule_once(runner.build, 0)
+        # Clock.schedule_once(runner.build, 0)
         Clock.schedule_interval(runner.update, 1.0 / 30.0)
         return runner
 
